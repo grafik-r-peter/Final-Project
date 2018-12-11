b0dbb9d80035a5496c0b0e9503a75417fa0f84dd
Roman PETER <grafik.r.peter@gmail.com>
2018-12-11 11:30:40 +0100

    stories frontend finished

--- inc/stories/actions/list_stories.php
+++ inc/stories/actions/list_stories.php
@@ -13,9 +13,20 @@ $join=array("student_profile.profile_id = stories.fk_profileID",
 			"student_skills.fk_skillsID = skills.skillsID",
 			"stories.storyID = images.fk_storiesID"
 
-			
 			);
 
 $rows=$obj->join_tables($tables,$rows,$join);
+
+$data=array();
+foreach ($rows as $row) {
+   $data[$row['profile_id']]["profile_id"] = $row["profile_id"];	
+   $data[$row['profile_id']]["first_name"] = $row["first_name"];
+   $data[$row['profile_id']]["last_name"] = $row["last_name"];
+   $data[$row['profile_id']]["image_url"] = $row["image_url"];
+   $data[$row['profile_id']]["story_content"] = $row["story_content"];
+   $data[$row['profile_id']]["story_teaser"] = $row["story_teaser"];
+   $data[$row["profile_id"]]["skills"][$row["skillsID"]]=array("skills"=>$row["skill"]);
+}
+
 
 ?>
--- stories.php
+++ stories.php
@@ -11,9 +11,10 @@ include "inc/stories/actions/list_stories.php"
 	<h1 class="title text-center p-3">Our Success Stories</h1>
 
 	<h4 class="title text-center p-4">Alumni from CodeFactory</h4>
-	
 
-	<?php foreach($rows as $row){ ?>	
+   
+
+	<?php foreach ($data as $row) { ?>	
 		<div class="row p-2">
 			<figure class="col-md-4 col-sm-12">
 				<figcaption class="text-uppercase font-weight-bold text-center">
@@ -48,20 +49,25 @@ include "inc/stories/actions/list_stories.php"
 
 			</figure>
 
-			<p class="col-md-4 col-sm-12 text-justify pt-md-4"><?php echo $row['story_content'] ?></p>
+			<p class="col-md-4 col-sm-12 text-justify pt-md-4"><?php echo $row['story_teaser'] ?></p>
 			<div class="col-md-4 col-sm-12 text-justify pt-md-4">
 				<p class="font-weight-bold">Skills:</p>
 				<ul>
-					<li><?php echo $row['skill'] ?></li>
+					 <?php 
+						$arr= $row["skills"];
+					   foreach ($arr as $c){
+					       echo "<li>".$c["skills"]."</li>";
+					   } 
+					 ?>
 
 			<br>
-					<button class="btn btn-outline-dark" type="button" data-toggle="collapse" data-target="#collapseExample321" aria-expanded="false" aria-controls="collapseExample321">
+					<button class="btn btn-outline-dark" type="button" data-toggle="collapse" data-target="#collapse<?php echo $row['profile_id'];?>" aria-expanded="false" aria-controls="collapse<?php echo $row['profile_id'];?>">
 		    		Read more
 		  			</button>
 				</ul>
 			</div>
 
-			<div class="collapse m-3" id="collapseExample321">
+			<div class="collapse m-3" id="collapse<?php echo $row['profile_id'];?>">
 				<div class="card card-body">
 					<?php echo $row['story_content']; ?>					
 				</div>
